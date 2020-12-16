ae29078b9ab881c6d097c0f0dcf9207959231824
Piotr <pksiazek.py@gmail.com>
2020-12-16 22:00:39 +0100

    Adding corresponding tracks from random album

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from typing import List
+import random
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from spotipy.oauth2 import SpotifyOAuth
 import json
 import pprint
+
 scope = "playlist-modify-public"
 my_uri = 'spotify:playlist:1qBB4GjHyKZYnQclPJfNe0'
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
@@ -35,7 +37,7 @@ def get_id_of_newest_playlist() -> str:
     return new_playlist['items'][0]['id']
 
 
-def get_new_track_list(artists_id_list: List[str], tracks_from_old_playlist: List[str]) -> List[str]:
+def get_new_listed_top_tracks(artists_id_list: List[str], tracks_from_old_playlist: List[str]) -> List[str]:
     track_list = []
     for artist_id in artists_id_list:
         top_tracks = spotify.artist_top_tracks(artist_id, country='PL')['tracks']
@@ -49,22 +51,39 @@ def get_new_track_list(artists_id_list: List[str], tracks_from_old_playlist: Lis
                 break # We want only one corresponding track
     return track_list
 
+def get_non_popular_tracks(artists_id_list: List[str], tracks_from_old_playlist: List[str]) -> List[str]:
+    track_list = []
+    for artist_index, artist_id in enumerate(artists_id_list):
+        while True:
+            albums = spotify.artist_albums(artist_id)['items']
+            number_of_albums = len(albums)
+            random_album_index = random.randint(0, number_of_albums-1)
+            random_album_id = albums[random_album_index]['id']
+            random_album = spotify.album_tracks(random_album_id)['items']
+            number_of_tracks = len(random_album)
+            random_track_index = random.randint(0, number_of_tracks-1)
+            random_track_id = random_album[random_track_index]['id']
 
-def create_list_of_tracks(playlist_id: str, tracks: List[str]) -> List[str]:
-    pass
+            if random_track_id in tracks_from_old_playlist: #We didn't find anything new
+                continue
+            else:
+                track_list.append(random_track_id)
+                break
+    return track_list
 
-old_playlist_id = 'spotify:playlist:2Pcd729IGTgs9QfkA7kjqe'
-def main():
-    # create_new_playlist('nowa_playlista')
+old_playlist_id = 'spotify:playlist:1qBB4GjHyKZYnQclPJfNe0'
+def create_new_playlist_from_not_mentioned_top_songs():
     new_playlist_id = get_id_of_newest_playlist()
     artists_from_old_playlist = get_playlist_items(old_playlist_id, 'artist')
-    # print(artists_from_old_playlist)
     tracks_from_old_playlist = get_playlist_items(old_playlist_id, 'track')
-    new_track_list = get_new_track_list(artists_from_old_playlist, tracks_from_old_playlist)
+    new_track_list = get_new_listed_top_tracks(artists_from_old_playlist, tracks_from_old_playlist)
     spotify.user_playlist_add_tracks(user=user, playlist_id=new_playlist_id, tracks=new_track_list)
 
-    # top_tracks = spotify.artist_top_tracks('spotify:artist:7M1FPw29m5FbicYzS2xdpi', country="PL")['tracks']
-    # top_track = top_tracks[4]['name']
-    # print(top_track)
+def create_new_playlist_from_corresponding_rando_tracks():
+    new_playlist_id = get_id_of_newest_playlist()
+    artists_from_old_playlist = get_playlist_items(old_playlist_id, 'artist')
+    tracks_from_old_playlist = get_playlist_items(old_playlist_id, 'track')
+    new_track_list = get_non_popular_tracks(artists_from_old_playlist, tracks_from_old_playlist)
+    spotify.user_playlist_add_tracks(user=user, playlist_id=new_playlist_id, tracks=new_track_list)
 
-main()
+create_new_playlist_from_corresponding_rando_tracks()
