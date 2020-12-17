9dec0af9e2395e6e0247710fd4ffde1960cc2428
Piotr <pksiazek.py@gmail.com>
2020-12-17 16:30:21 +0100

    updated config

--- main.py
+++ main.py
@@ -3,16 +3,13 @@ import random
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from spotipy.oauth2 import SpotifyOAuth
-import json
-import pprint
+import config
 
-scope = "playlist-modify-public"
-my_uri = 'spotify:playlist:1qBB4GjHyKZYnQclPJfNe0'
+scope = config.scope
+my_uri = config.my_uri
+user = config.user
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
-# 'spotify:playlist:1jfYYhj3hvaKkxilBvgx5l'
-pp = pprint.PrettyPrinter(indent=4)
-user='rbegouu2bjgv6t268sa2dc9hj'
 
 
 def get_playlist_items(playlist_id: str, type_of_item: str) -> List[str]:
@@ -51,6 +48,7 @@ def get_new_listed_top_tracks(artists_id_list: List[str], tracks_from_old_playli
                 break # We want only one corresponding track
     return track_list
 
+
 def get_non_popular_tracks(artists_id_list: List[str], tracks_from_old_playlist: List[str]) -> List[str]:
     track_list = []
     for artist_index, artist_id in enumerate(artists_id_list):
@@ -71,7 +69,10 @@ def get_non_popular_tracks(artists_id_list: List[str], tracks_from_old_playlist:
                 break
     return track_list
 
+
 old_playlist_id = 'spotify:playlist:2oHKnV8uHRPIyysDn5SjL0'
+
+
 def create_new_playlist_from_not_mentioned_top_songs():
     new_playlist_id = get_id_of_newest_playlist()
     artists_from_old_playlist = get_playlist_items(old_playlist_id, 'artist')
@@ -79,11 +80,13 @@ def create_new_playlist_from_not_mentioned_top_songs():
     new_track_list = get_new_listed_top_tracks(artists_from_old_playlist, tracks_from_old_playlist)
     spotify.user_playlist_add_tracks(user=user, playlist_id=new_playlist_id, tracks=new_track_list)
 
-def create_new_playlist_from_corresponding_rando_tracks():
+
+def create_new_playlist_from_corresponding_random_tracks():
     new_playlist_id = get_id_of_newest_playlist()
     artists_from_old_playlist = get_playlist_items(old_playlist_id, 'artist')
     tracks_from_old_playlist = get_playlist_items(old_playlist_id, 'track')
     new_track_list = get_non_popular_tracks(artists_from_old_playlist, tracks_from_old_playlist)
     spotify.user_playlist_add_tracks(user=user, playlist_id=new_playlist_id, tracks=new_track_list)
 
-create_new_playlist_from_corresponding_rando_tracks()
+
+create_new_playlist_from_corresponding_random_tracks()
