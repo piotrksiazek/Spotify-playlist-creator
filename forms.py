12c2e9d285e2f856414838e16966f77c8a533631
Piotr Ksiazek <pksiazek.py@gmail.com>
2020-12-30 21:39:34 +0100

    Add login template and functionality

--- forms.py
+++ forms.py
@@ -13,3 +13,16 @@ class OriginDestination2(FlaskForm):
     origin_playlist = SelectField('Origin', choices=[], validators=[DataRequired()])
     destination_playlist = SelectField('Destination', choices=['abc', 'scd'], validators=[DataRequired()])
     submit = SubmitField('MAKE PLAYLIST BROOO')
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    spotify_id = StringField('Spotify Id', validators=[DataRequired()])
+    submit = SubmitField('Sign In')
--- routes.py
+++ routes.py
@@ -1,26 +1,32 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, url_for, flash
 import config
 from spotipy.oauth2 import SpotifyOAuth
 import spotipy
 from config import Config
-from forms import OriginDestination, OriginDestination2
+from forms import OriginDestination, OriginDestination2, LoginForm
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager, UserMixin, current_user, login_user, logout_user, login_required
 from flask_apscheduler import APScheduler
+from werkzeug.security import generate_password_hash, check_password_hash
+from main import create_new_playlist_from_not_mentioned_top_songs
+
 
 app = Flask(__name__, template_folder='templates')
 app.config.from_object(Config)
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
 scheduler = APScheduler()
 
 scope = config.scope
 my_uri = config.my_uri
 user = config.user
 spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
-from main import create_new_playlist_from_not_mentioned_top_songs
+
 
 @app.before_first_request
 def start_scheduler():
@@ -32,15 +38,50 @@ def make_shell_context():
     return {'db': db, 'User': User}
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
+    spotify_id = db.Column(db.String(64), index=True, unique=False)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+@app.route('/logout')
+def logout():
+    if current_user.is_authenticated:
+        logout_user()
+        return redirect(url_for('index'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user_object = User.query.filter_by(username=form.username.data).first()
+        if user_object is None or not user_object.check_password(form.password.data):
+            print('Invalid username or password')
+            return redirect(url_for('index'))
+        login_user(user_object, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+
 
 @app.route('/', methods=['GET', 'POST'])
-def home():
+@app.route('/index')
+def index():
     form = OriginDestination()
     if form.validate_on_submit():
         original_playlist = form.origin_playlist.data
@@ -50,6 +91,7 @@ def home():
 
 
 @app.route('/MyPlaylists', methods=['GET', 'POST'])
+@login_required
 def my_playlists():
     # playlists = [(playlist['name'], playlist['id']) for playlist in spotify.user_playlists(user=user)['items']]
     playlists_dict = {}
